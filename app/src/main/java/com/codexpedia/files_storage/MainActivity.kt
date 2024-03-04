869ab9b59fd0008cefe512f05a632fce9a0d7c18
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2024-03-04 12:08:28 +0300

    Update MainActivity.kt

--- app/src/main/java/com/codexpedia/files_storage/MainActivity.kt
+++ app/src/main/java/com/codexpedia/files_storage/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.codexpedia.files_storage
 
 
-
+import androidx.appcompat.app.AppCompatActivity
   
 import android.content.Context  
-import android.support.v7.app.AppCompatActivity  
+
+//import android.support.v7.app.AppCompatActivity  
 import android.os.Bundle  
 import android.view.View  
 import android.widget.Button  
