b77b5dba26b7baa636e12ee45e917840985390d6
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2024-03-08 18:33:15 +0300

    Update MainActivity.kt

--- app/src/main/java/com/codexpedia/files_storage/MainActivity.kt
+++ app/src/main/java/com/codexpedia/files_storage/MainActivity.kt
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
           //getFilesStreamPath(name).getAbsolutePath()
             val fileOutputStream:FileOutputStream  
 
-var myExternalFile:File = File("/storage/19EB-032A/",fileName)  
+var myExternalFile:File = File("/storage/19EB-032A/",file)  
           
             try {  
                 fileOutputStream = openFileOutput( myExternalFile.toString(), MODE_PRIVATE)  
